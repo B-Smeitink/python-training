fb402947ec5b1df43588b1228d4f19f04cebe957
Baseltov <bassmeitink@hotmail.com>
2022-09-30 15:57:42 +0200

    Final push for GitHub actions

---
+++ src/pythontraining/d7_math.py
@@ -0,0 +1,28 @@
+# mathematics.py
+import math
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return round(x / y, 1)  # should be return round(x / y, 1)
+
+
+def round_up(x):
+    rounded = math.ceil(x)
+    return rounded
+
+
+def hypotenuse(x, y):
+    return math.sqrt(x ** 2 + y ** 2)
+
+
+def count_registrations(registrations):
+    return len(registrations)
+
+
+def create_attendee_list(registrations):
+    return [f"{r['first_name']} {r['last_name']}"
+            for r in registrations]
